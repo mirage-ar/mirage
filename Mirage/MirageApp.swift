7b21ec0ead41b75a4af5433509b55ad26d1b6e79
fiigmnt <fiigmnt@protonmail.com>
2023-01-09 17:21:09 -0500

    Initial Commit

---
+++ Mirage/MirageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MirageApp.swift
+//  Mirage
+//
+//  Created by fiigmnt on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MirageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
