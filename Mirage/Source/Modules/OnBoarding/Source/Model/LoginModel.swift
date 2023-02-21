7c9b01de0a249cd6f4ecfc4de958fcdffffd750f
Saad <saad.sial77@hotmail.com>
2023-02-21 17:47:29 +0500

    MIR-43: Initial Structure

---
+++ Mirage/Source/Modules/OnBoarding/Source/Model/LoginModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginModel.swift
+//  Mirage
+//
+//  Created by Saad on 21/02/2023.
+//
+
+import Foundation
+
+struct LoginModel {
+    
+}
---
+++ Mirage/Source/Modules/OnBoarding/Source/ViewModel/LoginViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewModel.swift
+//  Mirage
+//
+//  Created by Saad on 21/02/2023.
+//
+
+import Foundation
+
+struct LoginViewModel {
+    
+}
