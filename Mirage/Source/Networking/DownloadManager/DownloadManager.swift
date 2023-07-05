d3d3ac138d4dcb9f17a2af251eaf8b5246e10a7a
Saad <saad.sial77@hotmail.com>
2023-07-06 02:52:08 +0500

    Completed Download Manager

--- Mirage/Source/Networking/DownloadManager/DownloadManager.swift
+++ Mirage/Source/Networking/DownloadManager/DownloadManager.swift
@@ -21,7 +21,8 @@ public class DownloadManager {
         AF.sessionConfiguration.timeoutIntervalForResource = 30
         AF.sessionConfiguration.httpMaximumConnectionsPerHost = 10
         
-        if let dict = UserDefaults.standard.object(forKey: fileSetKey) as? [String: File] {
+        if let data = UserDefaults.standard.object(forKey: fileSetKey) as? Data,
+           let dict = try? JSONDecoder().decode([String: File].self, from: data) {
             fileSet = dict
             
         } else {
@@ -32,8 +33,11 @@ public class DownloadManager {
     
     func download(url: String, progressHandler: ((Progress) -> Void)?, completion: ((String?) -> ())?) {
         let queue = DispatchQueue(label: "downloadFiles", qos: .background, attributes: .concurrent)
+        if let file = fileSet[url], file.status == .completed, !file.filePath.isEmpty {
+            completion?(file.filePath)
+            return
+        }
         fileStarted(url: url, operation: .download)
-
         AF.request(
             url,
             method: .get,
@@ -52,7 +56,7 @@ public class DownloadManager {
                         self?.fileCompleted(url: url, filePath: fileUrl.absoluteString , operation: .download)
                         completion?(fileUrl.absoluteString)
                         } catch (let e){
-                            print("Error Saving File:\(fileUrl) Error:\(e)")
+                            debugPrint("Error Saving File:\(fileUrl) Error:\(e)")
                             completion?(nil)
                     }
                     print(fileUrl)
@@ -70,12 +74,17 @@ public class DownloadManager {
             try data?.write(to: fileUrl)
                 upload(filePath: fileName, completion: completion)
             } catch (let e){
-                print("Error Saving File:\(fileUrl) Error:\(e)")
+                debugPrint("Error Saving File:\(fileUrl) Error:\(e)")
                 completion?(nil)
         }
 
     }
     func upload(filePath: String, completion: ((String?) -> ())?) {
+        if let file = fileSet[filePath], file.status == .completed, !file.filePath.isEmpty {
+            //this is just to return url. to display. please use local file path to display
+            completion?(file.filePath)
+            return
+        }
         
         guard let fileUrl = URL(string: filePath) else { return }
         let queue = DispatchQueue(label: "downloadFiles", qos: .background, attributes: .concurrent)
@@ -91,19 +100,20 @@ public class DownloadManager {
 
             switch result {
             case .success(let url):
-                print(url)
+                debugPrint(url)
                 self.fileCompleted(url: filePath, filePath: url, operation: .upload)
                 completion?(url)
             case .failure(let encodingError):
                 self.fileFailed(url: filePath)
-                print(encodingError)
+                debugPrint(encodingError)
                 completion?(nil)
             }
         }
     }
     //MARK: File Actions
     private func synchronizeFileSet() {
-        UserDefaults.standard.set(fileSet, forKey: fileSetKey)
+        let data = try? JSONEncoder().encode(fileSet)
+        UserDefaults.standard.set(data, forKey: fileSetKey)
         UserDefaults.standard.synchronize()
     }
     private func fileStarted(url: String, operation: File.Operation) {
@@ -161,9 +171,18 @@ public class DownloadManager {
             file.lastRetry = nil
             fileSet[file.url] = file
             if file.operation == .download {
+                download(url: file.url) { progress in
+                    debugPrint("Progress \(file.url) \(progress)")
+                } completion: { filePath in
+                    debugPrint("Completed \(file.url) " + (filePath ?? ""))
+                }
+
                 download(url: file.url, progressHandler: nil, completion: nil)
             } else {
-                upload(filePath: file.filePath, completion: nil)
+                
+                upload(filePath: file.filePath) { url in
+                    debugPrint("Completed \(file.filePath) with \(url ?? "NA")")
+                }
             }
         }
         synchronizeFileSet()
@@ -181,20 +200,65 @@ struct File {
     var filePath: String = ""
     var status: Status = .notStarted
     var retries: Int = 0
-    let startedAt: Date = .now
+    var startedAt: Date = .now
     var completedAt: Date?
     var failedAt: Date?
     var lastRetry: Date?
     var priority: Priority = .medium
     let operation: Operation
     
-    enum Status: Int {
+    enum Status: Int, Codable {
         case notStarted = 0, inProgress, failed, completed
     }
-    enum Operation: Int {
+    enum Operation: Int, Codable {
         case download = 0, upload
     }
-    enum Priority: Int {
+    enum Priority: Int, Codable {
         case low = 0, medium = 1, high = 2
     }
+    
+}
+extension File: Codable {
+    enum CodingKeys: String, CodingKey {
+        case url
+        case filePath
+        case status
+        case retries
+        case startedAt
+        case completedAt
+        case failedAt
+        case lastRetry
+        case priority
+        case operation
+    }
+
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        url = try values.decode(String.self, forKey: .url)
+        filePath = try values.decode(String.self, forKey: .filePath)
+        status = try values.decode(Status.self, forKey: .status)
+        retries = try values.decode(Int.self, forKey: .retries)
+        startedAt = try values.decode(Date.self, forKey: .startedAt)
+        completedAt = try? values.decode(Date.self, forKey: .completedAt)
+        failedAt = try? values.decode(Date.self, forKey: .failedAt)
+        lastRetry = try? values.decode(Date.self, forKey: .lastRetry)
+        priority = try values.decode(Priority.self, forKey: .priority)
+        operation = try values.decode(Operation.self, forKey: .operation)
+    }
+
+    func encode(with encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(url, forKey: .url)
+        try container.encode(filePath, forKey: .filePath)
+        try container.encode(status, forKey: .status)
+        try container.encode(retries, forKey: .retries)
+        try container.encode(startedAt, forKey: .startedAt)
+        try container.encode(completedAt, forKey: .completedAt)
+        try container.encode(failedAt, forKey: .failedAt)
+        try container.encode(lastRetry, forKey: .lastRetry)
+        try container.encode(priority, forKey: .priority)
+        try container.encode(operation, forKey: .operation)
+    }
+
+
 }
