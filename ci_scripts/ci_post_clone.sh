19c62a10dfc4c2ddf74284b0adff9d23350651db
Saad <saad.sial77@hotmail.com>
2023-07-27 18:04:06 +0500

    ci post clon script with MAPBox authetnication

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+touch ~/.netrc
+
+echo "machine api.mapbox.com" >> ~/.netrc
+echo "login mapbox" >> ~/.netrc
+echo "password sk.eyJ1IjoiZmlpZ21udCIsImEiOiJjbDNqb2hlczUwMm5yM2NxYWxsdGRzajY3In0.XLac15xZ2LuKFFBbml7g6w" >> ~/.netrc
